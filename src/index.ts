79bd3dbc59b4f19adaba829bda24e7279164221e
Matthew Burkard <matthewburkard@protonmail.com>
2021-09-20 19:31:51 -0400

    feat: package for distribution
    Add index to house exports.
    Tweak config for distribution.

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import {JSONRPCClient} from "./client";
+import {
+  getErrorByCode,
+  InternalError,
+  InvalidParams,
+  InvalidRequest,
+  JSONRPCError,
+  MethodNotFound,
+  ParseError,
+  ServerError
+} from "./errors";
+import {RPCHTTPClient} from "./http_client";
+import {
+  ErrorObject,
+  ErrorResponseObject,
+  NotificationObject,
+  RequestObject,
+  ResultResponseObject
+} from "./objects";
+
+export {
+  JSONRPCClient,
+  RPCHTTPClient,
+  RequestObject,
+  NotificationObject,
+  ErrorObject,
+  ErrorResponseObject,
+  ResultResponseObject,
+  InternalError,
+  InvalidParams,
+  InvalidRequest,
+  JSONRPCError,
+  MethodNotFound,
+  ParseError,
+  ServerError,
+  getErrorByCode
+};
