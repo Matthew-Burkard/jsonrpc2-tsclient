050a97765785ac0b30753571f7511e2f6bdfbcaa
Matthew Burkard <matthewjburkard@gmail.com>
2021-12-13 19:43:40 -0500

    feat: manage ids instead of using random numbers

--- src/client.ts
+++ src/client.ts
@@ -7,16 +7,29 @@ import {getErrorByCode, ServerError} from "./errors.js";
 
 export abstract class JSONRPCClient {
 
-  genId(): number {
-    return Math.floor(Math.random() * 100000);
+  private ids: Map<number, number> = new Map();
+
+  private getId(): number {
+    let max = 0;
+    for (const it of this.ids.values()) {
+      if (it > max) {
+        max = it;
+      }
+    }
+    let newId = max + 1;
+    this.ids[newId] = newId;
+    return newId;
   }
 
   protected abstract sendAndGetJSON(request: RequestObject): Promise<any>
 
   protected async call(method: string, params?: any[] | object): Promise<any> {
-    let request = new RequestObject(this.genId(), method, params);
+    let request = new RequestObject(this.getId(), method, params);
     let data = await this.sendAndGetJSON(request);
     let response;
+    if (data.id !== undefined) {
+      this.ids.delete(data.id);
+    }
     if (data.result !== undefined) {
       response = Object.assign(new ResultResponseObject(), data);
       return response.result;
